ef762839332267e096f3b9ccba812627719b1489
yueyu27 <46236627+yueyu27@users.noreply.github.com>
2025-11-19 10:37:07 -0800

    Update Part5_additional_submission_data_cleaning.R

--- Part5_additional_submission_data_cleaning.R
+++ Part5_additional_submission_data_cleaning.R
@@ -22,8 +22,14 @@ library(tidyverse)
 library(stringr)
 
 
+#--------------
+#
+#  Benefit
+#
+#--------------
+
 # -- Read in data
-submit_raw <- read.delim("MEC_updated_report.txt",
+submit_raw <- read.delim("MEC_updated_report_with_benefit.txt",
                       header = TRUE,
                       colClasses = "character",
                       stringsAsFactors = FALSE)
@@ -56,6 +62,62 @@ dim(submit_2025)
 # 391 papers - submitted to ME with beenfit sharing statement
 tail(submit_2025)
 # code for last paper: MEC-25-0602 --> assume there are 602 paper submitted in total in 2025
+
+
+
+
+#--------------
+#
+#  Not Applicable
+#
+#--------------
+
+# -- Read in data
+submit_NA_raw <- read.delim("MEC_updated_report_with_NA.txt",
+                      header = TRUE,
+                      colClasses = "character",
+                      stringsAsFactors = FALSE)
+
+# -- Omit all rows without ID
+submit_NA_raw_2 <- submit_NA_raw[submit_NA_raw$ID != "",]
+
+# -- Omit all rows with revision
+submit_NA_raw_3 <- submit_NA_raw_2[!grepl("\\.R[0-9]+$", submit_NA_raw_2$ID), ]
+
+# -- Omit all duplicated ID rows 
+submit_NA_raw_4 <- submit_NA_raw_3[!duplicated(submit_NA_raw_3$ID), ]
+
+
+
+# -- Extract for 2023
+submit_NA_2023 <- submit_NA_raw_4[grep("MEC-23",submit_NA_raw_4$ID),]
+dim(submit_NA_2023)
+# 423 papers - submitted to ME stating N/A for complying with Nagoya Protocol
+
+ID_2023 <- submit_NA_2023$ID
+max(as.numeric(gsub("MEC-23-", "", ID_2023)))
+# 1242 --> last code in this file indicates there are 1242 paper submitted in total in 2023
+
+
+# -- Extract for 2024
+submit_NA_2024 <- submit_NA_raw_4[grep("MEC-24",submit_NA_raw_4$ID),]
+dim(submit_NA_2024)
+# 430 papers - submitted to ME stating N/A for complying with Nagoya Protocol
+
+ID_2024 <- submit_NA_2024$ID
+max(as.numeric(gsub("MEC-24-", "", ID_2024)))
+# 1425 --> last code in this file indicates there are 1425 paper submitted in total in 2024
+
+
+
+# -- Extract for 2025 (as of 2025 Nov 04)
+submit_NA_2025 <- submit_NA_raw_4[grep("MEC-25",submit_NA_raw_4$ID),]
+dim(submit_NA_2025)
+# 208 papers - submitted to ME stating N/A for complying with Nagoya Protocol
+
+ID_2025 <- submit_NA_2025$ID
+max(as.numeric(gsub("MEC-25-", "", ID_2025)))
+# 604 --> last code in this file indicates there are 604 paper submitted in total in 2025
 
 
 # END
